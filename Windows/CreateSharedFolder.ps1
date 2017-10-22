6f71c4837b983e32121c8026cca4bfa90bb2d678
Maxim Braekman <maxim.braekman@hotmail.com>
2017-10-22 14:31:36 +0200

    Windows-script

---
+++ Windows/CreateSharedFolder.ps1
@@ -0,0 +1,22 @@
+﻿# Create shared folders
+$Shares=[WMICLASS]"WIN32_Share"
+ 
+Function CreateSharedFolder ($Foldername, $Sharename) 
+{ 
+	# Create the folder if it does not yet exist 
+	IF (!(TEST-PATH $Foldername)) 
+	{ 
+		NEW-ITEM $Foldername -type Directory 
+	} 
+
+	# Double-check if the share doesn't exist before creating it
+	If (!(GET-WMIOBJECT Win32_Share -filter "Name='$Sharename'"))
+	{ 
+		$Shares.Create($Foldername,$Sharename,0) 
+		Write-Host $Sharename "created"
+	} 
+}
+ 
+ CreateSharedFolder -Foldername "D:\Share" -Sharename "SharedFolder"
+ 
+ 
