a0508f989d5453b603aafaa495ec7ff01758d3bf
Maxim Braekman <maxim.braekman@hotmail.com>
2019-02-22 09:34:23 +0100

    CopyFiles-script
    Script to copy all files from source to destination directory + update modified date + process-indicator

---
+++ Windows/CopyFiles.ps1
@@ -0,0 +1,23 @@
+﻿param(
+    [string]$sourceDirectory  = "",
+    [string]$destinationDirectory = ""
+)
+
+$sourceDirFileCount = $(Get-ChildItem $sourceDirectory | Measure-Object ).Count
+Write-Host "Source: " 
+Write-Host "$sourceDirectory [$sourceDirFileCount files]"
+Write-Host "Destination: "
+Write-Host $destinationDirectory
+
+$counter = 0
+Write-Output "Copying files:"
+foreach($file in Get-ChildItem $sourceDirectory) 
+{
+    $counter++
+    Write-Host "[$counter/$sourceDirFileCount] $($file.FullName)"
+    $filePath = $file.FullName
+    $fileName = $file.Name
+    Copy-Item -Force $filePath -Destination $destinationDirectory/$fileName
+    $(Get-Item $file.Fullname).lastwritetime=$(Get-Date)
+
+}
