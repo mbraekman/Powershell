c4b3dbeb5469a68170627fb2f8ab8263c32154e2
Maxim Braekman <maxim.braekman@hotmail.com>
2019-02-22 15:51:35 +0100

    CopyFiles
    Seperate script to copy files into destination folder and create x copies of that file (used for load testing)

---
+++ Windows/CopyFilesAndMultiply.ps1
@@ -0,0 +1,30 @@
+﻿param(
+    [string]$sourceDirectory  = "",
+    [string]$destinationDirectory = "",
+    [int]$amountOfCopies = 1,
+    [string]$extension = ".xml"
+)
+
+$sourceDirFileCount = $(Get-ChildItem $sourceDirectory | Measure-Object ).Count
+Write-Host "Source: " 
+Write-Host "$sourceDirectory [$sourceDirFileCount files]"
+Write-Host "Destination: "
+Write-Host $destinationDirectory
+
+$counter = 0
+$total = $sourceDirFileCount * $amountOfCopies
+Write-Output "Copying files:"
+foreach($file in Get-ChildItem $sourceDirectory) 
+{
+    for($i = 0; $i -lt $amountOfCopies;$i++)
+    {
+        $counter++
+        $filePath = $file.FullName
+        $fileName = $file.Name.Replace($file.Extension, '')
+        $outputFilePath = "$destinationDirectory/$($fileName)_$($i+1)$($file.Extension)"
+        Write-Host "[$counter/$total] $($outputFilePath)"
+
+        Copy-Item -Force $filePath -Destination $outputFilePath
+        $(Get-Item $outputFilePath).lastwritetime=$(Get-Date)
+    }
+}
