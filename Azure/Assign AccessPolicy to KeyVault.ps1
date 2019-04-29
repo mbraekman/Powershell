c65f9316ee50fa81e4b9e51ca1b287ed5c8af8fc
Maxim Braekman <maxim.braekman@hotmail.com>
2019-04-29 08:18:31 +0200

    Script to assign access to keyVault

---
+++ Azure/Assign AccessPolicy to KeyVault.ps1
@@ -0,0 +1,30 @@
+# The ID of the subscription in which the keyVault is located
+$subscriptionId = ''
+
+# The ID of the user-account to be granted permissions - to be found in the AD
+$userObjectId = ''
+
+# The name of the keyVault to which the user is to be granted access.
+$keyVaultName = ''
+
+# Below permissions match the default "Key, Secret & Certificate Management"-template.
+[String[]] $persmissionsToSecrets = ("get","list","set","delete","recover","backup","restore")
+[String[]] $permissionsToKeys = ("get","list","update","create","import","delete","recover","backup","restore")
+[String[]] $persmissionsToCertificates = ("get","list","update","create","import","delete","recover","backup","restore","Managecontacts","Getissuers","Listissuers","Setissuers","Deleteissuers","Manageissuers","Purge")
+
+# Login to Azure
+Login-AzureRmAccount
+
+# Select the correct subscription
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+
+try{
+    # Create an AccessPolicy for the specified user, with the specified permissions.
+    Set-AzureRmKeyVaultAccessPolicy -VaultName $keyVaultName -ObjectId $userObjectId -PermissionsToSecrets $permissionsToSecrets -PermissionsToKeys $permissionsToKeys -PermissionsToCertificates $permissionsToCertificates
+    Write-Information "Access policy has been created."
+}
+catch
+{
+    $errorMessage = $_.Exception.Message
+    Write-Error "Failed to create access policy: $errorMessage"
+}
