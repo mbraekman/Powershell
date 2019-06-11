605c75f58a5320e29ea937d4b07e34a356eb8eb6
Maxim Braekman <maxim.braekman@hotmail.com>
2019-06-11 09:13:28 +0200

    Update Azure-locks script
    Add comments to indicate what version of Azure PS to use from Azure DevOps release pipeline + uncomment actual remove-line

--- Azure/Remove All Locks In ResourceGroup.ps1
+++ Azure/Remove All Locks In ResourceGroup.ps1
@@ -1,10 +1,15 @@
-﻿$resourceGroupName = "%ResourceGroupName%"
+﻿# If executed from Azure DevOps release pipeline, set Azure PowerShell Version to 5.1.1
+# Currently, using the latest version will return 0 locks.
+
+param (
+    [Parameter(Mandatory=$true)][string]$resourceGroupName = $(throw "ResourceGroupName is required")
+)
 
 Write-Host "Retrieving locks in resourceGroup '$resourceGroupName'"
 
 $locks = Get-AzureRmResourceLock -ResourceGroupName $resourceGroupName
 
-if($locks.Count > 0)
+if($locks.Count -gt 0)
 {
     Write-Host "Start removing all locks in resourceGroup '$resourceGroupName'"
 
@@ -12,7 +17,7 @@ if($locks.Count > 0)
     {
         $lockId = $lock.LockId
         Write-Host "Removing the lock with ID:" $lockId
-        #Remove-AzureRmResourceLock -LockId $lockId -Force
+        Remove-AzureRmResourceLock -LockId $lockId -Force
     }
 
     Write-Host "All locks in resourceGroup '$resourceGroupName' have been removed"
