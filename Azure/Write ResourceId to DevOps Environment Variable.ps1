b98cbc25575e561922ac29dd8932213bcfc34bd2
Maxim Braekman <maxim.braekman@hotmail.com>
2019-12-24 11:26:19 +0100

    WriteResourceIdToDevOpsVariable
    Script can be used from withint Azure DevOps to retrieve the resourceId of a specific Azure Resource + write it to an Azure DevOps environment variable

---
+++ Azure/Write ResourceId to DevOps Environment Variable.ps1
@@ -0,0 +1,13 @@
+﻿param(
+   [string][parameter(Mandatory = $true)] $resourceGroupName,
+   [string][parameter(Mandatory = $true)] $resourceName,
+   [string][parameter(Mandatory = $true)] $variableName,
+   [string][parameter(Mandatory = $false)] $environmentVariableName = "ArmOutputs"
+)
+
+$resource = Get-AzureRmResource -ResourceGroupName $resourceGroupName -Name $resourceName
+
+$json = '{"' + $variableName + '": {"type":"string","value":"' + $resource.ResourceId + '"}}'
+
+Write-Host "##vso[task.setvariable variable=$environmentVariableName;]$json "
+Write-Host "Environment variable 'env:$environmentVariableName' has been set to $json"
