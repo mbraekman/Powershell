7b63ed374125f087316fe2aacd714bbd2986788a
Maxim Braekman <maxim.braekman@hotmail.com>
2020-03-23 17:05:41 +0100

    KeyVault Secret
    Provide script to upload a file (or certificate) as plain text to a KeyVault Secret (multiline support).

---
+++ Azure/Create KeyVault Secret - Certificate as PlainText.ps1
@@ -0,0 +1,31 @@
+﻿# Use this script to upload a certificate as plain text (multiline-support) into Azure KeyVault
+
+param (
+    [string][Parameter(Mandatory=$true)] $filePath = $(throw "The path to the file is required."),
+    [string][Parameter(Mandatory=$true)] $keyVaultName = $(throw "The path to the file is required."),
+    [string][Parameter(Mandatory=$true)] $secretName = $(throw "The path to the file is required."),
+    [bool][parameter(Mandatory = $false)] $loggedIn = $true,
+    [string][parameter(Mandatory = $false)] $subscriptionId = ""
+)
+
+if(-not($loggedIn))
+{
+    # Parameter indicates that the user has not been authenticated yet.
+    Write-Host('Logging in.')
+    Connect-AzureRmAccount -ErrorAction Stop
+    Write-Host('Logged in.')
+}
+if($subscriptionId)
+{
+    # SubscriptionId has been provided - switching to the required Azure Subscription
+    Write-Host('Selecting the subscription...')
+    Select-AzureRmSubscription -SubscriptionId $subscriptionId -ErrorAction Stop
+    Write-Host('Selected the subscription.')
+}
+
+# Perform the deployment based on the provided ARM-template and parameter file, if provided.
+Write-Host("Creating KeyVault secret...")
+
+Set-AzureKeyVaultSecret -VaultName $keyVaultName -SecretName $secretName -SecretValue (ConvertTo-SecureString (Get-Content $filePath -Raw) -force -AsPlainText ) -ErrorAction Stop
+
+Write-Host("Secret '$secretName' has been created.")
