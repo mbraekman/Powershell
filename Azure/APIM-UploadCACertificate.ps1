a178a259ca133af65e75af26974724468919393c
Maxim Braekman <maxim.braekman@hotmail.com>
2020-11-27 09:06:50 +0100

    APIM - Upload CA Certificates
    Provide script to upload public certificates into the CA-Certificates store within an APIM instance.
    To be improved: check if the certificate is already there -> don't continue.

---
+++ Azure/APIM-UploadCACertificate.ps1
@@ -0,0 +1,22 @@
+﻿param(
+   [string][parameter(Mandatory = $true)] $ResourceGroup = $(throw "Resource group is required"),
+   [string][parameter(Mandatory = $true)] $ServiceName = $(throw "API Management service name is required"),
+   [string][parameter(Mandatory = $true)] $CertificateFilePath = $(throw "Certificate file-path is required")
+
+)
+
+Write-Host "Loading Public Certificate"
+$rootCA = New-AzApiManagementSystemCertificate -StoreName "Root" -PfxPath $CertificateFilePath
+$systemCert = @($rootCa)
+
+Write-Host "Retrieving API Management Service Instance"
+$apimContext = Get-AzApiManagement -ResourceGroupName $ResourceGroup -Name $ServiceName
+$systemCertificates = $apimContext.SystemCertificates
+$systemCertificates += $systemCert
+$apimContext.SystemCertificates = $systemCertificates
+
+Write-Host "Uploading the SystemCertificate (This might take some time...)"
+    
+$result = Set-AzApiManagement -InputObject $apimContext -PassThru
+$result
+Write-Host "Finished"
