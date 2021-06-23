477bf93cdeb7d487e17df353fef9623dbb0fc910
Maxim Braekman <maxim.braekman@hotmail.com>
2021-06-23 17:06:27 +0200

    Create Register-Or-Unregister-PSRepository.ps1

---
+++ General/Register-Or-Unregister-PSRepository.ps1
@@ -0,0 +1,11 @@
+﻿# Register the MyGet repository so it can be accessed/used
+Register-PSRepository -Name MyGetCoditArcus -SourceLocation 'https://www.myget.org/F/arcus/api/v2/package'
+
+# Verify if the requested module can be found in the new repository
+Find-Module -Name Arcus.Scripting.Security -Repository MyGetCoditArcus
+
+# Install the module that is located in the new repository
+Install-Module Arcus.Scripting.DevOps -Repository MyGetCoditArcus
+
+# Unregister/remove the repository to avoid overlap with the PSGallery
+Unregister-PSRepository -Name MyGetCoditArcus
