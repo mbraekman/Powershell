6b90cd302927cb0de5dd8741c8e05cc5ddb4f489
Maxim Braekman <maxim.braekman@zure.com>
2022-01-19 16:07:46 +0100

    Add script to create key vault secret and disable the old versions

---
+++ Azure/Create KeyVault Secret - Disable old version.ps1
@@ -0,0 +1,32 @@
+﻿param (
+    [Parameter(Mandatory = $false)][string] $ResourceGroupName = "",
+    [Parameter(Mandatory = $true)][string] $KeyVaultName = $(throw "Name of the Azure Key Vault is required"),
+    [Parameter(Mandatory = $true)][string] $SecretName = $(throw "Name of the secret is required"),
+    [Parameter(Mandatory = $true)][string] $SecretValue = $(throw "Value of the secret is required")
+)
+
+$keyVault = $null
+if($ResourceGroupName -eq '') {
+    Write-Verbose "Looking for the Azure Key Vault with name '$KeyVaultName'..."
+    $keyVault = Get-AzKeyVault -VaultName $KeyVaultName
+} else {
+    Write-Verbose "Looking for the Azure Key Vault with name '$KeyVaultName' in resourcegroup '$ResourceGroupName'.."
+    $keyVault = Get-AzKeyVault -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName
+}
+
+# Get all current versions of the secret
+$secretVersions = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $SecretName -IncludeVersions
+
+Foreach ($secret in $SecretVersions)
+{
+    if($secret.Enabled)
+    {
+        Set-AzKeyVaultSecretAttribute -VaultName $KeyVaultName -Name $SecretName -Version $secret.Version -Enable $false
+        Write-Host "Disabled the secret with version " $secret.Version
+    }
+}
+
+# Add new secret value
+Write-Host 'Adding the new version of the secret'
+Set-AzKeyVaultSecret -VaultName $KeyVaultName -Name $SecretName -SecretValue ($SecretValue | ConvertTo-SecureString -AsPlainText -Force) -ErrorAction Stop
+Write-Host 'Added the new version of the secret'
